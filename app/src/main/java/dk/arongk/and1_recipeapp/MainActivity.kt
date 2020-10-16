a009657a964f23f2e27778c6f0cd866f1b34cdda
Aron Wising Kjærgaard <awk@usekeyhole.com>
2020-10-16 09:56:41 +0200

    chore(setup): initialize new project

---
+++ app/src/main/java/dk/arongk/and1_recipeapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package dk.arongk.and1_recipeapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
