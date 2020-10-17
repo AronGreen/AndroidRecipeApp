749704218f1b3ef3a3ba76e54ab57363a94db1e7
Aron Wising Kjærgaard <awk@usekeyhole.com>
2020-10-17 19:10:49 +0200

    Basic routing is up

--- app/src/main/java/dk/arongk/and1_recipeapp/MainActivity.kt
+++ app/src/main/java/dk/arongk/and1_recipeapp/MainActivity.kt
@@ -2,20 +2,26 @@ package dk.arongk.and1_recipeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity()  {
 //    val navController by lazy {findNavController(R.id.nav_host_fragment)}
 //    val appBarConfiguration by lazy {
 //        AppBarConfiguration(navController.graph, findViewById(R.id.bottom_navigation))
 //    }
-    val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
-    val navController = navHostFragment.navController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment_container)
+
+        NavigationUI.setupWithNavController(bottomNav,navController)
+
     }
+
 }
