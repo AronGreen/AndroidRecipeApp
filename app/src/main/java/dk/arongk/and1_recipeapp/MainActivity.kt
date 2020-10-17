a131c9245e6d3965a5def3a2f78f3c6db2dffdc2
Aron Wising Kjærgaard <awk@usekeyhole.com>
2020-10-17 10:43:19 +0200

    Setup fragments and navigation - untested

--- app/src/main/java/dk/arongk/and1_recipeapp/MainActivity.kt
+++ app/src/main/java/dk/arongk/and1_recipeapp/MainActivity.kt
@@ -1,9 +1,19 @@
 package dk.arongk.and1_recipeapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
+//    val navController by lazy {findNavController(R.id.nav_host_fragment)}
+//    val appBarConfiguration by lazy {
+//        AppBarConfiguration(navController.graph, findViewById(R.id.bottom_navigation))
+//    }
+    val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+    val navController = navHostFragment.navController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
